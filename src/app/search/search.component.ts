af67e140eecc579f72fdd7729320ad42da607375
Jun An <junimohano@gmail.com>
2017-03-28 18:37:46 -0400

    added clickmon

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import { YoutubeVideoComponent } from '../youtube-video/youtube-video.component';
 import { YoutubePlaylistComponent } from '../youtube-playlist/youtube-playlist.component';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
@@ -20,7 +21,20 @@ export class SearchComponent implements OnInit, AfterViewInit {
   minimumInputLength = 11;
   inputDebounceTime = 300;
 
-  constructor(private router: Router) { }
+  clickmonWidth: number;
+  clickmonHeight: number;
+  clickmonSrc: SafeResourceUrl;
+
+  constructor(private router: Router, private sanitizer: DomSanitizer) {
+    let isMobile = false;
+    if (/android|webos|iphone|ipad|ipod|blackberry|windows phone/i.test(navigator.userAgent)) {
+      isMobile = true;
+    }
+    this.clickmonWidth = isMobile ? 150 : 728;
+    this.clickmonHeight = isMobile ? 150 : 90;
+    // tslint:disable-next-line:max-line-length
+    this.clickmonSrc = this.sanitizer.bypassSecurityTrustResourceUrl(`${window.location.protocol}//${isMobile ? 'mtab' : 'tab2'}.clickmon.co.kr/pop/wp_${isMobile ? 'm_150' : 'ad_728'}.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1032700%7C%5E%7CAdver_M_1003115&mon_rf=REFERRER_URL`);
+  }
 
   ngOnInit() {
     if (this.router.url !== '/watch') {
