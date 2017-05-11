57b4d8b15737156393d2c0dd3c713d24fc3167f4
Jun An <junimohano@gmail.com>
2017-05-11 10:18:55 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { YoutubeApiService } from './shared/services/youtube-api.service';
-import * as FileSaver from 'file-saver';
-import { environment } from "environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -9,27 +6,8 @@ import { environment } from "environments/environment";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  /**
-   *
-   */
-  constructor(private youtubeApiService: YoutubeApiService) {
 
-  }
+  constructor() {
 
-  test() {
-
-    const save = document.createElement('a');
-    save.href = `${environment.webApiUrl}/api/v1/YoutubeConvert`;
-    save.target = '_blank';
-    // save.download = `${environment.webApiUrl}/api/v1/YoutubeConvert`;
-    const evt = document.createEvent('MouseEvents');
-    evt.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
-    save.dispatchEvent(evt);
-    (window.URL).revokeObjectURL(save.href);
-
-    // this.youtubeApiService.getDownload()
-    //   .subscribe(data => {
-    //     FileSaver.saveAs(new Blob([data]), 'output.mp4');
-    //   });
   }
 }
